a81d6215281d329bd2862cedb0c92cdb0f557da0
parvinderjitsf <parvinder.singh@sourcefuse.com>
2018-05-29 13:24:58 +0530

    - init with coder update

--- Framework/SwiftTagList/TagList.swift
+++ Framework/SwiftTagList/TagList.swift
@@ -85,7 +85,7 @@ open class TagList: UIView {
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     deinit {
